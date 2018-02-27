230eea8039b2ffbc444ef09e33f056307c794263
Afshin Akhgar <afshin.akhgar@gmail.com>
2018-02-27 12:02:54 +0330

    create seeds in command line

--- src/console/MakeSeederCommand.php
+++ src/console/MakeSeederCommand.php
@@ -11,32 +11,25 @@ use Kernel\Abstracts\AbstractConsole;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
-class MakeMigrationCommand extends AbstractConsole
+class MakeSeederCommand extends AbstractConsole
 {
     protected function configure()
     {
         $this
-            ->setName('make:migration')
+            ->setName('make:seed')
             ->setDescription('Generate Migration Class')
             ->addArgument(
                 'name',
                 InputArgument::REQUIRED,
-                'Migration name to Generate'
-            )
-            ->addArgument(
-                'column',
-                InputArgument::IS_ARRAY,
-                'column name (column:type) '
+                'seeder name to Generate'
             )
         ;
     }
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $names   = $input->getArgument('name');
-        $column = $input->getArgument('column');
-        $map ="";
-        $directory = 'database/migrations/';
-        $file = file_get_contents("resources/command_templates/create_migration.txt");
+        $directory = 'database/seeds/';
+        $file = file_get_contents("resources/command_templates/create_seeder.txt");
         $explodedArrName = explode('_',$names);
         $classNameNew ='';
         foreach($explodedArrName as $parted_names){
@@ -46,27 +39,19 @@ class MakeMigrationCommand extends AbstractConsole
         $file = str_replace("!name", ucfirst($classNameNew), $file);
         $file = str_replace("?name", strtolower($classNameNew), $file);
 
-        foreach ($column as $c) {
-            $entity = explode(":", $c);
-            $name   = $entity[0];
-            $type   = $entity[1];
-            //$map    .= "$table->".$type."('".$name."');".'\r\n';
-            $map    .= '$table->'.$type.'("'.$name.'");'."\n";
-        }
-        $file = str_replace("!table", $map, $file);
         $explodedArrName = explode('_',$names);
         $fileNameNew ='';
         foreach($explodedArrName as $parted_names){
             $fileNameNew .= ucfirst($parted_names);
         }
-        if (!file_exists($directory.date('YmdHis')."_".ucfirst($fileNameNew).".php")) {
-            $fh = fopen($directory .date('YmdHis')."_". ucfirst($fileNameNew) . ".php", "w");
+        if (!file_exists($directory.ucfirst($fileNameNew).".php")) {
+            $fh = fopen($directory .ucfirst($fileNameNew) . ".php", "w");
             fwrite($fh, $file);
             fclose($fh);
             $className = ucfirst($fileNameNew) . ".php";
-            $output->writeln("Created $className Migration in migrations");
+            $output->writeln("Created $className Seed in Seeds");
         } else {
-            $output->writeln("Class migration already Exists!");
+            $output->writeln("Class Seed already Exists!");
         }
 
     }
