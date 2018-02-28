1262e3999adec746eab92d9ae8068bb367ecea4d
Afshin Akhgar <afshin.akhgar@gmail.com>
2018-02-28 15:59:01 +0330

    add ip middleware

--- bootstrap/middlewares.php
+++ bootstrap/middlewares.php
@@ -1,5 +1,11 @@
 <?php
+
+$checkProxyHeaders = true; // Note: Never trust the IP address for security processes!
+$trustedProxies = ['10.0.0.1', '10.0.0.2']; // Note: Never trust the IP address for security processes!
+$app->add(new RKA\Middleware\IpAddress($checkProxyHeaders, $trustedProxies));
+
 $app->add(new \App\MiddleWare\AppMiddleWare($app->getContainer()));
 $app->add(new \Zeuxisoo\Whoops\Provider\Slim\WhoopsMiddleware($app));
 $app->add(new \App\MiddleWare\RouterMiddleWare($app->getContainer()));
 $app->add(new \App\MiddleWare\FlashMessageMiddleWare($app->getContainer()));
+
