bf2352ddbef81ae2b84138ada2b54df6e3f6e637
Afshin Akhgar <afshin.akhgar@gmail.com>
2018-03-03 16:18:28 +0330

    mailservice

--- kernel/Services/MailerService.php
+++ kernel/Services/MailerService.php
@@ -15,27 +15,7 @@ class MailerService extends AbstractContainer
 {
     public function init()
     {
-        $config = $this->settings['swiftmailer'];
-        $allowedTransportTypes = $config['allowed_transport_type'];
-        if (false === $config['enabled']
-            || !in_array($config['transport_type'], $allowedTransportTypes)
-        ) {
-            return false;
-        }
-        if ('smtp' === $config['transport_type']) {
-            $transport = new \Swift_SmtpTransport();
-        } elseif ('sendmail' === $config['transport_type']) {
-            $transport = new \Swift_SendmailTransport();
-        }
-        if (isset($config[$config['transport_type']])
-            && is_array($config[$config['transport_type']])
-            && count($config[$config['transport_type']])
-        ) {
-            foreach ($config[$config['transport_type']] as $optionKey => $optionValue) {
-                $methodName = 'set' . str_replace('_', '', ucwords($optionKey, '_'));
-                $transport->{$methodName}($optionValue);
-            }
-        }
-        return new \Swift_Mailer($transport);
+
+
     }
 }
