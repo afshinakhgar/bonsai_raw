4b55dc36479fcdd1c9dfc0657196f8982ef13671
Afshin Akhgar <afshin.akhgar@gmail.com>
2018-03-04 11:01:45 +0330

    rich directory heelper

--- kernel/Helpers/DirectoryHelper.php
+++ kernel/Helpers/DirectoryHelper.php
@@ -23,4 +23,38 @@ class DirectoryHelper
 
         return $filesInConfig;
     }
+
+
+    private static function create_class_array(string $fullpath, string $filename){
+
+        $class = $fullpath;
+        $class = str_replace("../","",$class);
+        $class = str_replace("app","App",$class);
+        $class = str_replace("/","\\",$class);
+        $class = str_replace(".php","",$class);
+        return ['class' => $class, 'class_name' => str_replace(".php","",$filename)];
+    }
+
+    public static function listFolderFiles($dir, &$results = array()){
+        if(is_dir($dir)){
+            $files = scandir($dir);
+        }
+
+        if(isset($files)){
+            foreach($files as $key => $value){
+                $path = $dir.DIRECTORY_SEPARATOR.$value;
+                if(!is_dir($path) && substr($value, -4) == '.php' && substr( $value, 0, 1 ) != "_") {
+                    $results[] = self::create_class_array($path,$value);
+                } else if($value != "." && $value != "..") {
+                    self::listFolderFiles($path, $results);
+                    if ( substr($path, -4) == '.php' && substr( $value, 0, 1 ) != "_") {
+                        $results[] = self::create_class_array($path,$value);
+                    }
+                }
+            }
+        }
+
+        return $results;
+    }
+
 }
