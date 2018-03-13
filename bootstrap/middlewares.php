e751af88d42d43c3c1bf680eba80b9608ca59e50
Afshin Akhgar <afshin.akhgar@gmail.com>
2018-03-13 11:56:08 +0330

    fix

--- bootstrap/middlewares.php
+++ bootstrap/middlewares.php
@@ -1,7 +1,7 @@
 <?php
 
 $checkProxyHeaders = true; // Note: Never trust the IP address for security processes!
-$trustedProxies = ['10.0.0.1', '10.0.0.2']; // Note: Never trust the IP address for security processes!
+$trustedProxies = ['127.0.0.1', '10.0.0.2']; // Note: Never trust the IP address for security processes!
 $app->add(new RKA\Middleware\IpAddress($checkProxyHeaders, $trustedProxies));
 
 $app->add(new \App\MiddleWare\AppMiddleWare($app->getContainer()));
