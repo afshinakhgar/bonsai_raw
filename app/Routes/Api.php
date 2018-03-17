7552eeb71726c2605f8e61e32772df9cd00cab9f
Afshin Akhgar <afshin.akhgar@gmail.com>
2018-03-17 14:37:29 +0330

    curl api call add to request  service

--- app/Routes/Api.php
+++ app/Routes/Api.php
@@ -2,5 +2,6 @@
 $app->group('/api', function ($app) use ($route) {
 	$app->group('/v1', function () use ($route) {
 		$route->post('/user/auhtenticate/login', \App\Controller\Api\V1\User\Authenticate::class.':login')->setName('api.login');
+		$route->post('/user/auhtenticate/register', \App\Controller\Api\V1\User\Authenticate::class.':register')->setName('api.register');
 	});
 });
--- kernel/Services/RequestService.php
+++ kernel/Services/RequestService.php
@@ -86,4 +86,82 @@ class RequestService extends AbstractServices
 
     }
 
+
+
+    /**
+     * curl get content
+     * method GET
+     * @param string $url
+     * @return mixed
+     */
+    public static function get_apiCall(string $url)
+    {
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+            CURLOPT_RETURNTRANSFER => 1,
+            CURLOPT_URL => $url,
+        ));
+
+        curl_setopt($curl, CURLOPT_CONNECTTIMEOUT ,0);
+        curl_setopt($curl, CURLOPT_TIMEOUT, 400); //timeout in seconds
+
+        $resp = curl_exec($curl);
+
+        $curl_errno = curl_errno($curl);
+        $curl_error = curl_error($curl);
+        curl_close($curl);
+
+        if ($curl_errno > 0) {
+            echo "cURL Error ($curl_errno): $curl_error\n";
+        } else {
+            return $resp;
+        }
+    }
+
+
+    /**
+     * @param string $path
+     * @return mixed
+     */
+    public function xml_curl_call(string $path){
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL,$path);
+        curl_setopt($ch, CURLOPT_FAILONERROR,1);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION,1);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
+        curl_setopt($ch, CURLOPT_TIMEOUT, 4);
+        $retValue = curl_exec($ch);
+        curl_close($ch);
+        return $retValue;
+    }
+
+
+    function post_apiCall($url, $data=NULL, $headers = NULL, $basicAuth = NULL) {
+        $ch = curl_init($url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+        if(!empty($data)){
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+        }
+
+        if (!empty($headers)) {
+            curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        }
+
+        if (!empty($basicAuth)) {
+            curl_setopt($ch, CURLOPT_USERPWD, $basicAuth['username'] . ":" . $basicAuth['password']);
+            curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+        }
+
+        $response = curl_exec($ch);
+
+        if (curl_error($ch)) {
+            trigger_error('Curl Error:' . curl_error($ch));
+        }
+
+        curl_close($ch);
+        return $response;
+    }
+
 }
