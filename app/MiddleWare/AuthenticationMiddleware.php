7fdbcdb1b70c62ccf096db719779c8ba93ef405e
Afshin Akhgar <afshin.akhgar@gmail.com>
2018-04-12 12:33:47 +0430

    add user middlewares

---
+++ app/MiddleWare/AuthenticationMiddleware.php
@@ -0,0 +1,28 @@
+<?php
+namespace App\Middleware;
+
+use Kernel\Facades\Auth;
+
+use Kernel\Abstracts\AbstractMiddleWare;
+
+class AuthenticationMiddleware extends AbstractMiddleWare
+{
+
+    /**
+     * @param \Psr\Http\Message\ServerRequestInterface $request
+     * @param \Psr\Http\Message\ResponseInterface      $response
+     * @param callable                                 $next
+     * @return \Psr\Http\Message\ResponseInterface
+     */
+    public function __invoke($request, $response, $next )
+    {
+        if(!Auth::check()){
+            return $response->withRedirect('/login');
+        }
+
+        $response = $next($request, $response);
+
+
+        return $response;
+    }
+}
---
+++ app/MiddleWare/UserOwnerMiddleware.php
@@ -0,0 +1,36 @@
+<?php
+namespace App\Middleware;
+
+use Kernel\Abstracts\AbstractMiddleWare;
+
+class UserOwnerMiddleWare extends AbstractMiddleWare
+{
+
+    /**
+     * @param \Psr\Http\Message\ServerRequestInterface $request
+     * @param \Psr\Http\Message\ResponseInterface      $response
+     * @param callable                                 $next
+     * @return \Psr\Http\Message\ResponseInterface
+     */
+    public function __invoke($request, $response, $next )
+    {
+
+        $uri          = $request->getUri();
+        $current_path = $uri->getPath();
+        $partUrl = explode('/', trim($current_path,'/'));
+        
+        
+
+       $routeParams = $request->getAttribute('routeInfo')[2];
+
+
+        if(!Auth::user()->id && !Auth::user()->id != $routeParams['id']){
+            return $response->withRedirect('/login');
+        }
+
+        $response = $next($request, $response);
+
+
+        return $response;
+    }
+}
