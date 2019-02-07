161f6366c79a19581fc926e5435c65a5b0edba98
afshin <afshin.akhgar@gmail.com>
2019-02-07 11:27:43 +0330

    fix

--- resources/view/new/admin/layout/main.blade.php
+++ resources/view/new/admin/layout/main.blade.php
@@ -5,21 +5,22 @@
     @include('admin.includes.head')
 
 </head>
-<body class="skin-blue sidebar-mini">
+<body class="skin-blue sidebar-mini sidebar-open">
 <div class="wrapper">
-    @include('admin.includes.header')
-    @include('admin.includes.aside')
+@include('admin.includes.header')
+@include('admin.includes.aside')
 
 
 <!-- Content Wrapper. Contains page content -->
     <div class="content-wrapper">
 
-        <section>
+        <section class="content">
             @include('includes.messages')
 
             @yield('content')
         </section>
     </div>
+</div>
 
 @include('admin.includes.footer')
 
